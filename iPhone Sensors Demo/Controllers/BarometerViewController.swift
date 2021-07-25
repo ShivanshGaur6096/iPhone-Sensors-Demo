25d5f1cc9825b3051600b625b35d4a48e4633243
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2021-07-25 21:46:42 +0200

    Added a new view controller for the Barometer sensor.

---
+++ iPhone Sensors Demo/Controllers/BarometerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BarometerViewController.swift
+//  iPhone Sensors Demo
+//
+//  Created by Aitor Zubizarreta on 25/07/2021.
+//
+
+import UIKit
+
+class BarometerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
