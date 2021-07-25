4c1273a8756c5a963e481adf7b46bc2ba391e93c
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2021-07-25 21:40:26 +0200

    Added a UITableView to the main view controller and created a method to setup its delegate and data source.

--- iPhone Sensors Demo/Controllers/MainViewController.swift
+++ iPhone Sensors Demo/Controllers/MainViewController.swift
@@ -8,11 +8,49 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
+    // MARK: - UI Elements
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: - Properties
+    
+    // MARK: - Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.setupTableView()
     }
+    
+    ///
+    /// Setup the Table View.
+    ///
+    private func setupTableView() {
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+    }
+}
 
+// MARK: - UITable View Delegate
+
+extension MainViewController: UITableViewDelegate {}
+
+// MARK: - UITable View Data Source
+
+extension MainViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell()
+        cell.textLabel?.text = "Cell title"
+        return cell
+    }
 }
