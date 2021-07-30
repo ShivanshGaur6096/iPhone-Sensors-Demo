280279f24b94fcad639f3a91cb3abce28a385d38
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2021-07-30 20:53:36 +0200

    Added a method in Gyroscope view controller to setup and get the data from the sensor.

--- iPhone Sensors Demo/Controllers/GyroscopeViewController.swift
+++ iPhone Sensors Demo/Controllers/GyroscopeViewController.swift
@@ -6,13 +6,24 @@
 //
 
 import UIKit
+import CoreMotion
 
 class GyroscopeViewController: UIViewController {
-
+    
+    // MARK: - UI Elements
+    
+    // MARK: - Properties
+    
+    let gyroscopeManager = CMMotionManager()
+    
+    // MARK: - Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.setupView()
+        
+        self.setupGyroscope()
     }
     
     ///
@@ -20,6 +31,25 @@ class GyroscopeViewController: UIViewController {
     ///
     private func setupView() {
         self.title = "Gyroscope"
+    }
+    
+    ///
+    /// Setup the Gyroscope sensor.
+    ///
+    private func setupGyroscope() {
+        if self.gyroscopeManager.isGyroAvailable {
+            // Set the data update interval.
+            self.gyroscopeManager.gyroUpdateInterval = 1
+            
+            // Start Gyroscope sensor data readout.
+            self.gyroscopeManager.startGyroUpdates(to: OperationQueue.main) { (data, error) in
+                if let data = data {
+                    print("Gyro x : \(data.rotationRate.x)")
+                    print("Gyro y : \(data.rotationRate.y)")
+                    print("Gyro z : \(data.rotationRate.z)")
+                }
+            }
+        }
     }
     
 }
